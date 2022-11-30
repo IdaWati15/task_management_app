987ecf29b6d7ae23637cac480ec0dc994247510a
IdaWati15 <idaw9765@gmail.com>
2022-11-30 16:49:12 +0700

    yt 13 Google Sign In Android & Web

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:tas_management_app/app/data/controller/auth_controller.dart';
 import 'package:tas_management_app/app/routes/app_pages.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
+final authC =Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +110,7 @@ class LoginView extends GetView<LoginController> {
     height: Get.height * 0.5, 
     ),
     FloatingActionButton.extended(
-      onPressed: () => Get.toNamed(Routes.HOME), 
+      onPressed: () => authC.signInWithGoogle(),
       label: const Text('Sign In With Google'),
       icon: const Icon(
       Ionicons.logo_google,
