4bda98c9bb69a45aa89b7a812f54c28bc8615731
IdaWati15 <idaw9765@gmail.com>
2023-01-01 22:08:21 +0700

    Commit yt 19 Add Data Task to Firestore Database

--- lib/app/modules/task/views/task_view.dart
+++ lib/app/modules/task/views/task_view.dart
@@ -1,3 +1,4 @@
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -109,67 +110,75 @@ class TaskView extends GetView<TaskController> {final GlobalKey<ScaffoldState> _
                         clipBehavior: Clip.antiAlias,
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-                      return  Container(
-                       //  width: 400,
-                       height: 160,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
-                      color: AppColors.cardBg, ),
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                        Row(
-           children:[
-                         ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: const CircleAvatar(
-                                backgroundColor: Colors.amber, 
-                                radius: 20, 
-                                foregroundImage: NetworkImage(
-                                      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?quality=75&width=982&height=726&auto=webp%27',
-               ),
-               ),
-               ),
-               ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: const CircleAvatar(
-                                backgroundColor: Colors.amber, 
-                                radius: 20, 
-                                foregroundImage: NetworkImage(
-                                      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?quality=75&width=982&height=726&auto=webp%27',
-               ),
-               ),
-               ),
-               Spacer(),
-               Container(
-                 height: 25,
-                 width: 80,
-                 color: AppColors.primaryBg,
-                 child: Center(child: Text('100 %', style: TextStyle(color: AppColors.primaryText,
-                 ),
-                 ),
-                 ),
-               ),
-               
-           ],
-           ),
-           Spacer(),
-            Container(
-                 height: 25,
-                 width: 80,
-                 color: AppColors.primaryBg,
-                 child: Center(child: Text('10 / 10 Task', style: TextStyle(color: AppColors.primaryText,
-                 ),
-                 ),
-                 ),
-               ),  
-               Text('Pemograman Mobile', style: TextStyle(color: AppColors.primaryText,fontSize: 20),
-               ),
-               Text('Deadline 2 hari lagi', style: TextStyle(color: AppColors.primaryText,fontSize: 15),
-               ),
-              ]),
-              );
+                      return  GestureDetector(
+                        onLongPress: () {
+                          addEditTask(
+                            context: context,
+                            type: 'Update', 
+                            docId: '2022-07-28T00:58:36.883963');
+                        },
+                        child: Container(
+                         //  width: 400,
+                         height: 160,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
+                        color: AppColors.cardBg, ),
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                          Row(
+                                 children:[
+                           ClipRRect(
+                                  borderRadius: BorderRadius.circular(25),
+                                  child: const CircleAvatar(
+                                  backgroundColor: Colors.amber, 
+                                  radius: 20, 
+                                  foregroundImage: NetworkImage(
+                                        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?quality=75&width=982&height=726&auto=webp%27',
+                                     ),
+                                     ),
+                                     ),
+                                     ClipRRect(
+                                  borderRadius: BorderRadius.circular(25),
+                                  child: const CircleAvatar(
+                                  backgroundColor: Colors.amber, 
+                                  radius: 20, 
+                                  foregroundImage: NetworkImage(
+                                        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?quality=75&width=982&height=726&auto=webp%27',
+                                     ),
+                                     ),
+                                     ),
+                                     Spacer(),
+                                     Container(
+                                       height: 25,
+                                       width: 80,
+                                       color: AppColors.primaryBg,
+                                       child: Center(child: Text('100 %', style: TextStyle(color: AppColors.primaryText,
+                                       ),
+                                       ),
+                                       ),
+                                     ),
+                                     
+                                 ],
+                                 ),
+                                 Spacer(),
+                                  Container(
+                                       height: 25,
+                                       width: 80,
+                                       color: AppColors.primaryBg,
+                                       child: Center(child: Text('10 / 10 Task', style: TextStyle(color: AppColors.primaryText,
+                                       ),
+                                       ),
+                                       ),
+                                     ),  
+                                     Text('Pemograman Mobile', style: TextStyle(color: AppColors.primaryText,fontSize: 20),
+                                     ),
+                                     Text('Deadline 2 hari lagi', style: TextStyle(color: AppColors.primaryText,fontSize: 15),
+                                     ),
+                                    ]),
+                                    ),
+                      );
                     },
                    ),
                   ),
@@ -186,21 +195,7 @@ class TaskView extends GetView<TaskController> {final GlobalKey<ScaffoldState> _
           alignment: Alignment(0.95 , 0.95),
           child: FloatingActionButton.extended(
             onPressed: (){
-              Get.bottomSheet(
-                Container(
-                  margin: context.isPhone 
-                  ? EdgeInsets.zero
-                  : const EdgeInsets.only(left: 150, right: 150),
-                  height: Get.height,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20), 
-                      topRight: Radius.circular(20), 
-                      ), 
-                    color: Colors.white,
-                  ),
-              ),
-              );
+              addEditTask(context: context, type: 'Add',docId: '');
             },
             label: Text('Add Task'), 
             icon: Icon(Ionicons.add),
@@ -208,4 +203,120 @@ class TaskView extends GetView<TaskController> {final GlobalKey<ScaffoldState> _
         ),
        );
   }
+
+    addEditTask({BuildContext? context, String? type, String? docId}){
+      Get.bottomSheet(
+                SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                    margin: context!.isPhone 
+                    ? EdgeInsets.zero
+                    : const EdgeInsets.only(left: 150, right: 150),
+                    height: Get.height,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20), 
+                        topRight: Radius.circular(20), 
+                        ), 
+                      color: Colors.white,
+                    ),
+                    child: Form(
+                      key: controller.formKey,
+                      child: Column(
+                    children: [
+                      Text(
+                        '$type Task',
+                        style: TextStyle(
+                          color: AppColors.primaryText, 
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Title',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      controller: controller.titleController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Cannot be empty';
+                        }
+                        return null;
+                      },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                        hintText: 'Description',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      controller: controller.descriptionController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Cannot be empty';
+                        }
+                        return null;
+                      },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      DateTimePicker(
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2100),
+                        dateLabelText: 'Due Date',
+                        decoration: InputDecoration(
+                          hintText: 'Due Date',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      controller: controller.dueDateController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty)  {
+                            return 'Can not be empty';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ConstrainedBox(
+                        constraints: 
+                          BoxConstraints.tightFor(width: Get.width, height: 40),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.saveUpdateTask(
+                            controller.titleController.text,
+                            controller.descriptionController.text,
+                            controller.dueDateController.text,
+                            docId!,
+                            type,
+                            );
+                        }, 
+                        child: Text(type!),
+                        ),
+                        ),
+                        SizedBox(
+                        height: 20,
+                      ),
+                      ],
+                      )),
+                              ),
+                ),
+              );
+    }
 }
